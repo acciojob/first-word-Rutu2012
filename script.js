5cb9470d4cd888b19590e8a3f01a2394bd8b60fd
Rutu2012 <sanghanirutu944@gmail.com>
2025-05-12 10:40:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function firstWord(s) {
-  // your code here
+  if (!str) return ""; // handle empty string
+
+    const spaceIndex = str.indexOf(" ");
+    if (spaceIndex === -1) {
+        return str; // no space found, return entire string
+    }
+
+    return str.substring(0, spaceIndex);
+}
+console.log(firstWord("see and stop"));
+console.log(firstWord("Hello World"));
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+// const s = prompt("Enter String:");
+// alert(firstWord(s));
